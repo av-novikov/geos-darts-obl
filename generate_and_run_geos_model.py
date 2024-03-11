f8e5c641bab2eca79125e0da0e65ad29ea088241
Aleks Novikov <a.novikov@tudelft.nl>
2024-03-11 21:02:58 +0100

    Fix the local folder name

--- generate_and_run_geos_model.py
+++ generate_and_run_geos_model.py
@@ -6,11 +6,11 @@ from scripts.geosx_obl_operators_object import OBLTableGenerator
 
 def generate_geos_input_file(darts_folder, model_folder, regenerate_obl_table=True):
     # Load fluid physics from DARTS model
-    Model = load_module_from_path(folder_path=physics_folder, module_name='model', entity_name='Model')
+    Model = load_module_from_path(folder_path=darts_folder, module_name='model', entity_name='Model')
     darts_model = Model(n_points=50)
 
     # Generate OBL table and write in a file
-    obl_table_file_name = physics_folder + 'obl_table.txt'
+    obl_table_file_name = darts_folder + 'obl_table.txt'
     if not os.path.exists(obl_table_file_name) or regenerate_obl_table:
         obl_table_gen = OBLTableGenerator(darts_model)
         obl_table_gen.generate_table()
@@ -18,7 +18,7 @@ def generate_geos_input_file(darts_folder, model_folder, regenerate_obl_table=Tr
 
     # Generate GEOS input *.xml
     generate = load_module_from_path(folder_path=model_folder, module_name='generate_xml', entity_name='generate')
-    generate(darts_model=darts_model, model_folder=model_folder, physics_folder=physics_folder)
+    generate(darts_model=darts_model, model_folder=model_folder, physics_folder=darts_folder)
 
 # Generate OBL tables & GEOS input file
 physics_folder = './darts_physics/_2ph_comp/'
